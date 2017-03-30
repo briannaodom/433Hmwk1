c19e60bd049fecbf9f479a32a22bdbdb4123ec2e
Brianna Odom <briannaodom2017@u.northwestern.edu>
2017-03-29 23:33:28 -0500

    made chances to loop in int main

--- hmwk1/433Hmwk1.X/main.c
+++ hmwk1/433Hmwk1.X/main.c
@@ -5,7 +5,7 @@
 #pragma config DEBUG = 0b11 // no debugging (disabled)
 #pragma config JTAGEN = 0 // no jtag (disabled)
 #pragma config ICESEL = 0b11 // use PGED1 and PGEC1
-#pragma config PWP = 0b111111111 // no write protect ()
+#pragma config PWP = OFF // no write protect ()
 #pragma config BWP = 1 // no boot write protect (writeable)
 #pragma config CP = 1 // no code protect
 
@@ -20,45 +20,56 @@
 #pragma config WDTPS = 0b10100 // use slowest wdt
 #pragma config WINDIS = 1 // wdt no window mode
 #pragma config FWDTEN = 0 // wdt disabled
-#pragma config FWDTWINSZ = 11 // wdt window at 25% //0b11?
+#pragma config FWDTWINSZ = 0b11 // wdt window at 25% //0b11?
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
-#pragma config FPLLIDIV = 0b001 // divide input clock to be in range 4-5MHz; resonater 8MHZ so divide by 2
+#pragma config FPLLIDIV = 0xb001 // divide input clock to be in range 4-5MHz; resonater 8MHZ so divide by 2
 #pragma config FPLLMUL = 0b111 // multiply clock after FPLLIDIV; used highest mult
 #pragma config FPLLODIV = 0b001 // divide clock after FPLLMUL to get 48MHz
 #pragma config UPLLIDIV = 0b001  // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
 #pragma config UPLLEN = 0 // USB clock on (enabled)
 
 // DEVCFG3
-#pragma config USERID = 0 // some 16bit userid, doesn't matter what
-#pragma config PMDL1WAY = x // allow multiple reconfigurations
-#pragma config IOL1WAY = x // allow multiple reconfigurations
-#pragma config FUSBIDIO = x // USB pins controlled by USB module
-#pragma config FVBUSONIO = x // USB BUSON controlled by USB module
+#pragma config USERID = 0xFFFF // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = 0 // allow multiple reconfigurations
+#pragma config IOL1WAY = 0 // allow multiple reconfigurations
+#pragma config FUSBIDIO = 1 // USB pins controlled by USB module
+#pragma config FVBUSONIO = 1 // USB BUSON controlled by USB module
 
 
 int main() {
 
     __builtin_disable_interrupts();
 
-    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+   //  set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
     __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
 
     // 0 data RAM access wait states
-    BMXCONbits.BMXWSDRM = 0x0;
+   BMXCONbits.BMXWSDRM = 0x0;
 
     // enable multi vector interrupts
-    INTCONbits.MVEC = 0x1;
+   INTCONbits.MVEC = 0x1;
 
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
-
+    TRISA = 0x0000; //TRISAbits.TRISA4 = 0;
+    TRISB = 0xFFFF; //TRISBbits.TRISB4 = 1;
+   
+    //LATAbits.LATA4 = 1;
     __builtin_enable_interrupts();
+    
+    int j = 0;
 
     while(1) {
+    
+       LATA = 0xFFFF;
+       for(j=0; j<10000; j++);
+       LATA = 0x0000;
+       for(j=0; j<10000; j++);
+       
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 		  // remember the core timer runs at half the sysclk
-    }
+  }
 }
