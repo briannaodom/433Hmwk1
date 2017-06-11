9ffa4c390876ac00b6f799c5b7542b2d4038484d
Brianna Odom <briannaodom2017@u.northwestern.edu>
2017-06-11 10:07:56 -0500

    got HID mouse to work

---
+++ hmwk11.1/firmware/src/dsp.h
@@ -0,0 +1,11 @@
+/* 
+ */
+
+#ifndef DSP_H
+#define	DSP_H
+
+//MAF
+float maf(float data[], int size);
+float iir(float data[], float old_avg);
+float fir(float data[], int size);
+#endif	/* I2CLIB_H */
